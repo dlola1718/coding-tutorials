0bd05a48c15fde299b65a806955d46e14dcb6a13
dami <dlola1718@gmail.com>
2021-08-04 16:21:50 +0100

    C-SHARP

---
+++ C#_Tutorial/CSHARP/CSHARP/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //DATA TYPES
+            string text = "here is a smaple text";
+            char grade = 'A';
+            int sampleNum = 30;
+            //bool isMale = True;
+            //float n = 2.0;
+            //decimal currency = 30.05;
+
+
+
+            //DRAWING A SHAPE
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  | ");
+            Console.WriteLine("/___| ");
+
+
+
+            //WORKING WITH STRING
+            Console.WriteLine("Enter your name: ");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Enter your Age: ");
+            String age = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + ". you are " + age + " years old.");
+        }
+    }
+}
---
+++ C#_Tutorial/program.cs
@@ -0,0 +1,18 @@
+//Drawing a shape
+
+
+namespace C_TUTORIAL
+{
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+    }
+
+    }
+    
+    
+}
