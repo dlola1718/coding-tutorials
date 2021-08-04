7d208e22e1f2e576a1da8d483f15a3a3572301de
dami <dlola1718@gmail.com>
2021-08-04 18:11:08 +0100

    simple calculator

---
+++ C#_Tutorial/SimpleCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
